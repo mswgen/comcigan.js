892ad2fbe9e112a572d6e693c5a42e10a52e9fe9
mswgen <54498184+mswgen@users.noreply.github.com>
2024-02-27 12:58:43 +0900

    export SchoolList

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { searchSchool } from "./search.js";
 import { getTimetable } from "./get.js";
 import { TimetableError, SearchError } from "./types.js";
-import type { Timetable } from "./types.js";
+import type { Timetable, SchoolList } from "./types.js";
 
 export default { searchSchool, getTimetable, TimetableError, SearchError };
-export type { Timetable };
+export type { Timetable, SchoolList };
